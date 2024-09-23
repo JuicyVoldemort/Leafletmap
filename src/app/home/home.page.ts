58f8fb84dc32bc2ee02618f55984a3963626780a
JuicyVoldemort <calvinrosmuhammadramadhino@mail.ugm.ac.id>
2024-09-23 10:54:01 +0700

    Initial Commit - Build pertama sesuai modul

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  map!: L.Map;
+
+  constructor() {}
+  ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.map = L.map('mapId').setView([51.505, -0.09], 10);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+
+
+}
